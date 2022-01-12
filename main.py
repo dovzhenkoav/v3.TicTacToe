3dd9a4fb231a4ba0ff628e15b898b8747e41f6e3
Artemii Dovzhenko <rockenny@ymail.com>
2022-01-12 22:57:09 +0300

    Changed download link. Herokuapp is shit actually.

--- main.py
+++ main.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/download', methods=['GET', 'POST'])
-def download():
-    return send_from_directory('static', path="files/TicTacToe.zip", filename="files/TicTacToe.zip")
-
 if __name__ == '__main__':
     app.run(debug=True)
